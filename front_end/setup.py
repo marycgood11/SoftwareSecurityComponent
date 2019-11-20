4bc7e08af795d8382009efe8bd6f2e73340e4e24
Mary Catherine Good <marycatherine@Marys-MBP.lan1>
2019-11-20 03:45:34 -0500

    Took out old code from setup

--- front_end/setup.py
+++ front_end/setup.py
@@ -40,39 +40,3 @@ class UI_Setup(tk.Tk):
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
-
-
-
-"""class UI_Setup(Tk):
-    # INITIALIZES THE BASE FRAME AND THE LIST OF FRAMES IN THE APPLICATION
-    def __init__(self, master):
-        # INITILIZES THE BASE FRAME
-        self.master = master
-        self.master.title("SONY Security Awareness Training")
-        self.master.geometry("550x650")
-        # Layout Frame Containers
-        self.master.grid_rowconfigure(0, weight=1)
-        self.master.grid_columnconfigure(1, weight=1)
-
-        Frame.__init__(self, master)
-
-        # WILL HOLD ALL FRAMES AS THEY ARE PLACED ON TOP OR TAKEN AWAY DURING NAVIGATION
-        container = Frame(self.master, bg="#4A87FF")
-        container.pack(fill="both", expand=True)
-
-        self.frames = {}
-
-        # KEEPS TRACK OF ALL FRAMES THAT CAN BE NAVIGATED TO
-        # ADD NEW FRAMES TO THIS LIST
-        for F in (welcomepage.WelcomePage, homepage.HomePage):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(welcomepage.WelcomePage)
-
-    # NAVIGATES TO A NEW FRAME
-    def show_frame(self, page):
-        frame = self.frames[page]
-        frame.tkraise()
-"""
